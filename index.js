db82b40eb5f2c32a0f089fff14a534758f199c43
shivam <mandjoai123@gmail.com>
2024-03-30 19:13:39 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
+// import dotenv from 'dotenv'
+// dotenv.config()
+// import express from 'express'
+// import cors from 'cors'
+// import router from './routes/useRoutes.js'
+// import mongoose from "mongoose";
+// import cookieParser from 'cookie-parser'
+// import formData from 'express-form-data'
+// async function connectDb(DATABASE_URL) {
+//     try {
+//         const DB_options = {
+//             dbname: "shivam"
+//         };
+//         await mongoose.connect(DATABASE_URL, DB_options);
+//         console.log("Database Connected........");
+//     } catch (error) {
+//         console.log(error);
+//     }
+
+
+// }
+
+// const app = express()
+// const port=process.env.PORT
+// const DATABASE_URL=process.env.DATABASE_URL
+
+// connectDb(DATABASE_URL)
+
+// const corsOptions = {
+//     credentials: true,
+//     origin: 'http://localhost:5173'
+//   };
+  
+// app.use(cors(corsOptions));
+// app.use(express.json())
+// app.use(cookieParser())
+// app.use(formData.parse());
+
+// app.use("/api/users",router)
+// app.listen(port,()=>{
+//     console.log(`listening port on ${port}`)
+// })
+
+
+
+
 import dotenv from 'dotenv'
 dotenv.config()
 import express from 'express'
@@ -6,6 +52,8 @@ import router from './routes/useRoutes.js'
 import mongoose from "mongoose";
 import cookieParser from 'cookie-parser'
 import formData from 'express-form-data'
+import bodyParser from 'body-parser'
+import expressSession from 'express-session'
 async function connectDb(DATABASE_URL) {
     try {
         const DB_options = {
@@ -27,17 +75,32 @@ const DATABASE_URL=process.env.DATABASE_URL
 connectDb(DATABASE_URL)
 
 const corsOptions = {
+     methods: ['POST', 'GET', 'DELETE', 'PUT'],
     credentials: true,
-    origin: 'http://localhost:5173'
+    origin: 'http://localhost:5173',
+    exposedHeaders: ["set-cookie"],
   };
-  
-
-
-
 app.use(cors(corsOptions));
 app.use(express.json())
 app.use(cookieParser())
 app.use(formData.parse());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(
+  session({
+    key: 'id',
+    secret:  process.env.jwt_secret_key,
+    resave: false,
+    saveUninitialized:false,
+    name: 'session',
+    proxy: true,
+    cookie: {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none',
+      maxAge: 1000 * 60 * 60 * 24,
+    }
+  })
+    )
 
 app.use("/api/users",router)
 app.listen(port,()=>{
