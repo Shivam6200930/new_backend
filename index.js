3c61e02b1a92470b76b59273242e5bd024b2f0f3
shivam <mandjoai123@gmail.com>
2024-05-24 14:07:30 +0530

    url-hide

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const DATABASE_URL = process.env.DATABASE_URL;
 connectDb(DATABASE_URL);
 
 const corsOptions = {
-  origin:'*',
+  origin:process.env.frontend_url,
   credentials: true,
   exposedHeaders: ['Set-Cookie'],
 };
